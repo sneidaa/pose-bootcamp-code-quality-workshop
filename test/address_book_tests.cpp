09eed00be74163a65ec5146346afc339ec78e592
andresch <andreas.schneider@mrtplm.com>
2024-05-08 09:16:00 +0000

    additional test cases added

--- test/address_book_tests.cpp
+++ test/address_book_tests.cpp
@@ -11,3 +11,21 @@ TEST_CASE("entries can be added and removed") {
 	ab.remove_entry("Jane Doe");
 	CHECK_FALSE(ab.has_entry("Jane Doe"));
 }
+
+TEST_CASE("attempting to add an entry with a name violating these restrictions throws an exception'") {
+	address_book ab;
+	CHECK_THROWS_WITH(ab.add_entry("mynameismorethan100characterslong_mynameismorethan100characterslong_mynameismorethan100characterslong"), "Name too long");
+}
+
+TEST_CASE("get_entries returns a list of all entries, sorted alphabetically'") {
+	address_book ab;
+	ab.add_entry("B");
+	ab.add_entry("A");
+	CHECK(ab.get_entries()[0] == "A");
+}
+
+TEST_CASE("get_entries names returned by the address book have the first letter of each word capitalized'") {
+	address_book ab;
+	ab.add_entry("bbbb");
+	CHECK(ab.get_entries()[0] == "Bbbb");
+}
